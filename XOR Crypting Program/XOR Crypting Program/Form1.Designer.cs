8142866b749e5470e72dea09f4c820e83226a4bc
Filip Rogowski <mail@rogowski.ovh>
2018-01-28 05:51:29 +0100

    v1

--- XOR Crypting Program/XOR Crypting Program/Form1.Designer.cs
+++ XOR Crypting Program/XOR Crypting Program/Form1.Designer.cs
@@ -29,32 +29,138 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(278, 85);
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(22, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(131, 53);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Otwórz plik z danymi wejściowymi";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(183, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(664, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zapisz plik";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(183, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(664, 178);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Zapisz hasło do pliku";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Wczytaj hasło";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Hasło:";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(183, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(664, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Wykonaj szyfrowanie";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(22, 153);
+            this.richTextBox2.MaxLength = 3000;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(131, 96);
+            this.richTextBox2.TabIndex = 12;
+            this.richTextBox2.Text = "";
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(183, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(664, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Wykonaj deszyfrowanie";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(674, 185);
+            this.ClientSize = new System.Drawing.Size(870, 285);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "XOR Crypting Program";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- XOR Crypting Program/XOR Crypting Program/Form1.cs
+++ XOR Crypting Program/XOR Crypting Program/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace XOR_Crypting_Program
 {
@@ -15,6 +16,137 @@ namespace XOR_Crypting_Program
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string inputFile;
+        private string outputFile;
+        private string passwordFile;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readInputFile();
+        }
+
+        //Wczytywanie danych wejściowych 
+        private void readInputFile()
+        {      
+            OpenFileDialog openInputFile = new OpenFileDialog();
+            openInputFile.Title = "Otwórz plik z danymi wejściowymi";
+            openInputFile.Filter = "Text files (*.txt)|*.txt";
+            if (openInputFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(File.OpenRead(openInputFile.FileName));
+
+                inputFile = read.ReadToEnd();
+                read.Dispose();
+                richTextBox1.Text = richTextBox1.Text + "<<Wczytano dane wejściowe o długości "+ inputFile.Length +">>\n";
+                if (inputFile.Length < 30)
+                {
+                    richTextBox1.Text = richTextBox1.Text + "<<" + inputFile + ">>\n";
+                }
+                outputFile = inputFile;
+                button1.Enabled = false;
+                button5.Enabled = true;
+            }
+        }
+
+        //Zapisowanie pliku wyjściowego 
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Zapisz plik z danymi wyjściowymi";
+            save.Filter = "Text files (*.txt)|*.txt";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter write = new StreamWriter(File.Create(save.FileName));
+
+                write.Write(outputFile);
+                write.Dispose();
+                richTextBox1.Text = "<<Zapisano plik z danymi wyjściowymi>>\n";
+
+                button2.Enabled = false;
+                button1.Enabled = true;
+                button6.Enabled = false;
+                button5.Enabled = false; 
+            }
+        }
+
+        //Wykonywanie szyfrowania
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                outputFile = doXor(outputFile, richTextBox2.Text);
+                richTextBox1.Text = richTextBox1.Text + "<<Dane zaszyfrowane>>\n";
+                if (outputFile.Length < 30)
+                {
+                    richTextBox1.Text = richTextBox1.Text + "<<" + outputFile + ">>\n";
+                }
+                button5.Enabled = false;
+                button6.Enabled = true;
+                button2.Enabled = true;
+            }
+            catch
+            {
+                richTextBox1.Text = richTextBox1.Text + "<<Plik wejściowy oraz hasło muszą zawierać przynajmniej jeden znak.>>\n";
+            }
+        }
+
+        //Szyfrowanie
+        private string doXor(string inputFile, string inputPassword)
+        {
+                char[] input = inputFile.ToArray();
+                char[] password = inputPassword.ToArray();
+                char[] output = new char[input.Length];
+                for (int i = 0; i < input.Length; i++)
+                {
+                    output[i] = (char)(input[i] ^ password[i % password.Length]);
+                }
+                return new string(output);
+        }
+
+
+        //Zapisywanie hasła do pliku
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            SaveFileDialog save = new SaveFileDialog();
+            save.Title = "Zapisz plik z hasłem";
+            save.Filter = "Password files (*.password)|*.password";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter write = new StreamWriter(File.Create(save.FileName));
+
+                write.Write(richTextBox2.Text);
+                write.Dispose();
+                richTextBox1.Text = richTextBox1.Text + "<<Zapisano plik z hasłem>>\n";
+            }
+        }
+        
+        //Wczytywanie hasła 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openPasswordFile = new OpenFileDialog();
+            openPasswordFile.Title = "Otwórz plik z hasłem";
+            openPasswordFile.Filter = "Password files (*.password)|*.password";
+            if (openPasswordFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(File.OpenRead(openPasswordFile.FileName));
+
+                passwordFile = read.ReadToEnd();
+                read.Dispose();
+                richTextBox2.Text = passwordFile;
+                richTextBox1.Text = richTextBox1.Text + "<<Wczytano hasło o długości " + passwordFile.Length + ">>\n";
+            }
+        }
+
+        //Deszyfrowanie pliku
+        private void button6_Click(object sender, EventArgs e)
+        {
+            outputFile = doXor(outputFile, richTextBox2.Text);
+            richTextBox1.Text = richTextBox1.Text + "<<Plik został rozszyfrowany.>>\n";
+            richTextBox1.Text = richTextBox1.Text + "<<" + outputFile + ">>\n";
+            button6.Enabled = false;
+            button5.Enabled = true; 
         }
     }
 }
